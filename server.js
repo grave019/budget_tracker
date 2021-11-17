1abbecc644447e81508872422f1f051f3a54abd3
Brent Graves <graves.brent@gmail.com>
2021-11-17 17:27:54 -0500

    updated server.js

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = process.env.PORT || 3000;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+const PORT = 3000;
 
 const app = express();
 
@@ -16,10 +15,16 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+// Mongo deployment info
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/budget", 
+  {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
-});
+  }
+);
 
 // routes
 app.use(require("./routes/api.js"));
