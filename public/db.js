23d41147de3ae2bf770e08784d4cacce4f5cabab
Brent Graves <graves.brent@gmail.com>
2021-11-17 17:22:03 -0500

    added function to check db

--- public/db.js
+++ public/db.js
@@ -1,9 +1,11 @@
 // declare global variables 
 let db;
 let budgetVersion;
+
 //initializing the database
 const request = indexedDB.open('BudgetDB', budgetVersion || 21);
 
+//check for changes to the database
 request.onupgradeneeded = function (event) {
     console.log('Upgrade needed in IndexDB');
     const {
@@ -19,6 +21,43 @@ request.onupgradeneeded = function (event) {
         db.createObjectStore('BudgetStore', {
           autoIncrement: true
         });
-      }
-    
+      }   
     };
+    request.onerror = function (event) {
+        console.log(`${event.target.errorCode}`)
+      };
+      
+      function checkDB() {
+        console.log('check db invoked');
+      
+        let transaction = db.transaction(['BudgetStore'], 'readwrite');
+      
+        const store = transaction.objectStore('BudgetStore');
+      
+        const getAll = store.getAll();
+        getAll.onsuccess = function () {
+
+            if (getAll.result.length > 0) {
+              fetch('/api/transaction/bulk', {
+                  method: "POST",
+                  body: JSON.stringify(getAll.result),
+                  headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                  },
+                })
+                .then((response) => response.json())
+                .then((res) => {
+                  if (res.length !== 0) {
+        
+                    transaction = db.transaction(['BudgetStore'], 'readwrite');
+        
+                    const currentStore = transaction.objectStore('BudgetStore');
+        
+                    currentStore.clear();
+                    console.log('Clearing store');
+                  }
+                });
+            }
+          }
+        }
