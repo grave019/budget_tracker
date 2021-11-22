cd3c82f7e76386ed01a133b71619af21f82c9b2f
Brent Graves <graves.brent@gmail.com>
2021-11-22 13:52:22 -0500

    trying to fix app not saving offline.

--- public/js/db.js
+++ public/js/db.js
@@ -1,24 +1,31 @@
 let db;
 let budgetVersion;
-const request = indexedDB.open('BudgetDB', budgetVersion);
+
+const request = indexedDB.open('BudgetDB', budgetVersion || 21);
+
 request.onupgradeneeded = function (e) {
   console.log('Upgrade needed in IndexDB');
+
   const { oldVersion } = e;
   const newVersion = e.newVersion || db.version;
-  console.log(`DB Updated from v ${oldVersion} to ${newVersion}`);
+  console.log(`DB Updated from version ${oldVersion} to ${newVersion}`);
   db = e.target.result;
   if (db.objectStoreNames.length === 0) {
     db.createObjectStore('BudgetStore', { autoIncrement: true });
   }
 };
+
 request.onerror = function (e) {
-  console.log(`Error status: ${e.target.errorCode}`);
+  console.log(`Woops! ${e.target.errorCode}`);
 };
-function checkDB() {
+
+function checkDatabase() {
   console.log('check db invoked');
+
   let transaction = db.transaction(['BudgetStore'], 'readwrite');
   const store = transaction.objectStore('BudgetStore');
   const getAll = store.getAll();
+
   getAll.onsuccess = function () {
     if (getAll.result.length > 0) {
       fetch('/api/transaction/bulk', {
@@ -35,24 +42,27 @@ function checkDB() {
             transaction = db.transaction(['BudgetStore'], 'readwrite');
             const currentStore = transaction.objectStore('BudgetStore');
             currentStore.clear();
-            console.log('Clearing out the store 🧹');
+            console.log('Clearing store 🧹');
           }
         });
     }
   };
 }
+
 request.onsuccess = function (e) {
-  console.log('onsuccess successful');
+  console.log('success');
   db = e.target.result;
   if (navigator.onLine) {
-    console.log('Backend up and at em! 🗄️');
-    checkDB();
+    console.log('Backend online! 🗄️');
+    checkDatabase();
   }
 };
+
 const saveRecord = (record) => {
-  console.log('Save record has been triggered');
+  console.log('Save record invoked');
   const transaction = db.transaction(['BudgetStore'], 'readwrite');
   const store = transaction.objectStore('BudgetStore');
   store.add(record);
 };
-window.addEventListener('online', checkDB);
+
+window.addEventListener('online', checkDatabase);
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,67 +1,65 @@
-const FILES_TO_CACHE = [
-    '/',
-    '/index.html',
-    '/styles.css',
-    '/icons/icon-192x192.png',
-    '/icons/icon-512x512.png',
-    '/db.js',
-    '/index.js'
-  ];
-  const PRECACHE = 'precache-v1';
-  const RUNTIME = 'runtime';
-  self.addEventListener('install', (event) => {
-    event.waitUntil(
-      caches
-        .open(PRECACHE)
-        .then((cache) => cache.addAll(FILES_TO_CACHE))
-        .then(self.skipWaiting())
+console.log('This is your service-worker.js file!');
+
+const FILES_TO_CACHE = [ `/db.js`, `/index.js`, `/index.html`, `/styles.css`, `/manifest.webmanifest`, `/icons/money.png` ];
+
+const STATIC_CACHE = `static-cache-v1`;
+const RUNTIME_CACHE = `runtime-cache`;
+
+self.addEventListener(`install`, e => {
+    e.waitUntil(
+        caches
+            .open(STATIC_CACHE)
+            .then(cache => cache.addAll(FILES_TO_CACHE))
+            .then(() => self.skipWaiting())
     );
-  });
-self.addEventListener("activate", function(evt) {
-    evt.waitUntil(
-      caches.keys().then(keyList => {
-        return Promise.all(
-          keyList.map(key => {
-            if (key !== PRECACHE && key !== RUNTIME) {
-              console.log("Removing old cache data", key);
-              return caches.delete(key);
-            }
-          })
-        );
-      })
+});
+
+self.addEventListener(`activate`, e => {
+    const currentCaches = [STATIC_CACHE, RUNTIME_CACHE];
+    e.waitUntil(
+        caches
+            .keys()
+            .then(cacheNames => 
+                cacheNames.filter(cacheName => !currentCaches.includes(cacheName))
+            ) .then (deleteCaches => 
+                Promise.all(
+                    deleteCaches.map(deleteCaches => caches.delete(deleteCaches))
+            )
+        ) .then(() => self.clients.claim())
     );
-  
-    self.clients.claim();
-  });
-  
-  // fetch
-  self.addEventListener("fetch", function(evt) {
-    // cache successful requests to the API
-    if (evt.request.url.includes("/api/")) {
-      evt.respondWith(
-        caches.open(RUNTIME).then(cache => {
-          return fetch(evt.request)
-            .then(response => {
-              // If the response was good, clone i and store it in the cache.
-              if (response.status === 200) {
-                cache.put(evt.request.url, response.clone());
-              }
-              return response;
-            })
-            .catch(err => {
-              // Network request failed, try to get it from the cache.
-              return cache.match(evt.request);
-            });
-        }).catch(err => console.log(err))
-      );
-  
-      return;
+});
+
+self.addEventListener(`fetch`, e => {
+    if (
+        e.request.method !== `GET` || !e.request.url.startsWith(self.location.origin)
+    ) {
+        e.respondWith(fetch(e.request));
+        return;
     }
-    // if the request is not for the API, serve static assets using "offline-first" approach.
-    // see https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook#cache-falling-back-to-network
-    evt.respondWith(
-      caches.match(evt.request).then(function(response) {
-        return response || fetch(evt.request);
-      })
+
+    if (e.request.url.includes(`/api/transaction`)) {
+        e.respondWith(
+            caches.open(RUNTIME_CACHE).then(cache =>
+                fetch(e.request)
+                    .then (response => {
+                        cache.put(e.request, response.clone());
+                        return response;
+                }).catch(() => caches.match(e.request))
+            )
+        );
+        return;
+    }
+    e.respondWith(
+        caches.match(e.request).then(cachedResponse => {
+            if(cachedResponse) {
+                return cachedResponse;
+            }
+            return caches
+                .open(RUNTIME_CACHE)
+                .then(cache =>
+                    fetch(e.request).then(response =>
+                        cache.put(e.request, response.clone()).then(() => response)
+                    ));
+        })
     );
-  });
+});
